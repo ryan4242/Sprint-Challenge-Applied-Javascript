ef99919e770a326ecaa54eece139fc7baa076f51
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-05-15 19:01:54 -0700

    cards are complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,44 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    for (let [key, value] of Object.entries(response.data.articles)) {
+        value.forEach(article => {
+            cardContainer.append(cardMaker(article));
+        })
+    }
+})
+.catch(err => {
+    cardContainer.innerHTML = 'Oh-no we hit a snag! Check the console log for details.';
+    console.log(err);
+})
+
+const cardMaker = (article) => {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    card.append(headline);
+    card.append(author);
+    author.append(imgContainer);
+    imgContainer.append(image);
+    author.append(authorName);
+
+    headline.innerHTML = article.headline;
+    image.src = article.authorPhoto;
+    authorName.innerHTML = `By ${article.authorName}`;
+
+    return card;
+};
+
+const cardContainer = document.querySelector('.cards-container');
